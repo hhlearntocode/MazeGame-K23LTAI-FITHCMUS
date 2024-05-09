5a0e16df5f7944283cad439f28c0227c0e727527
hhlearntocode <146340670+hhlearntocode@users.noreply.github.com>
2024-05-09 16:02:17 +0700

    Add files via upload

---
+++ MazeGame-master/main.py
@@ -0,0 +1,13 @@
+#import pygame
+#import menu
+#import mazeGeneration as mg
+from sys import exit
+from authentication import *
+
+
+if __name__ == "__main__":
+    page()
+
+# Đổi tên thành MAZE GAME 
+# ĐỔI GIAO DIỆN NHÌN CỔ ĐIỂN, PIXEL
+# Thêm PAUSE
